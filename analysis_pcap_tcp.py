6e535dc833cb2006dd1eef4623471110ad63ea6d
stevenhsin <steven.hsin@stonybrook.edu>
2020-03-08 12:33:28 -0400

    added method to get source IP

--- analysis_pcap_tcp.py
+++ analysis_pcap_tcp.py
@@ -4,4 +4,23 @@ f = open('assignment2.pcap', 'rb')
 pcap = dpkt.pcap.Reader(f)
 
 for ts, buf in pcap:
-    print(ts, len(buf))
+    eth = dpkt.ethernet.Ethernet(buf)
+    ip = eth.data
+    tcp = ip.data
+
+print(eth)
+print(ip.src)
+print(tcp)
+
+
+# returns the source IP address
+def get_src_ip(src_in_hex):
+    src_ip = ''
+    for num in src_in_hex:
+        src_ip = src_ip + str(num) + "."
+
+    src_ip = src_ip[0:src_ip.__len__() - 1]
+    return(src_ip)
+
+
+f.close()
