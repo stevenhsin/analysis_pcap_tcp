a7e2f9973c879c3f4e5c4865d51b72877c3dda27
stevenhsin <steven.hsin@stonybrook.edu>
2020-03-08 13:11:31 -0400

    generalized getting IP

--- analysis_pcap_tcp.py
+++ analysis_pcap_tcp.py
@@ -1,26 +1,29 @@
 import dpkt
 
-f = open('assignment2.pcap', 'rb')
+
+# returns the source/destination IP address
+def get_ip(ip_in_hex):
+    new_ip = ''
+    for num in ip_in_hex:
+        new_ip = new_ip + str(num) + "."
+
+    new_ip = new_ip[0:new_ip.__len__() - 1]
+    return new_ip
+
+
+def analysis_pcap_tcp():
+    for ts, buf in pcap:
+        eth = dpkt.ethernet.Ethernet(buf)
+        ip = eth.data
+        tcp = ip.data
+
+    print(eth)
+    print(ip.src)
+    print(tcp)
+
+
+file_name = 'assignment2.pcap'  # sys.argv[1]
+f = open(file_name, 'rb')
 pcap = dpkt.pcap.Reader(f)
-
-for ts, buf in pcap:
-    eth = dpkt.ethernet.Ethernet(buf)
-    ip = eth.data
-    tcp = ip.data
-
-print(eth)
-print(ip.src)
-print(tcp)
-
-
-# returns the source IP address
-def get_src_ip(src_in_hex):
-    src_ip = ''
-    for num in src_in_hex:
-        src_ip = src_ip + str(num) + "."
-
-    src_ip = src_ip[0:src_ip.__len__() - 1]
-    return(src_ip)
-
 
 f.close()
